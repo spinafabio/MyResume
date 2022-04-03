6acd028a60abe13fb99916b115cf2b312fbe4653
Fabiospi <fabio.spinelli@shipsta.com>
2022-04-03 20:57:42 +0200

    redirecting routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './core/components/layout/layout.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: '',
     component: LayoutComponent,
